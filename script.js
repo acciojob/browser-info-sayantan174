eb5e30b87191681c9c92da15db1659d355e33c0e
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-04-16 14:09:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let displayText = "You are using "+navigator.appName+"version "+navigator.appVersion
+console.log(displayText)
+const body = document.getElementsByTagName("body")[0];
+const div = document.createElement("div")
+div.innerText=displayText;
+body.appendChild(div);
